43985d0141ebd3579b1b60efc960a4e308a0ee34
ioio-tech <movieofgames@gmail.com>
2025-07-23 16:14:03 +0800

    feat: implemented SceneLoader

--- Assets/AppName_Rename/Scripts/ViewController/InitializationLoader.cs
+++ Assets/AppName_Rename/Scripts/ViewController/InitializationLoader.cs
@@ -1,36 +1,16 @@
-using System;
 using AppName_Rename.Core;
-using Cysharp.Threading.Tasks;
 using QFramework;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.SceneManagement;
 
 public class InitializationLoader : MonoBehaviour, IController
 {
-    [SerializeField] AssetReference _gameplayScene;
+    [SerializeField] SceneLoader _sceneLoader;
 
     void Start()
     {
-        LoadGameplayScene().Forget();
-    }
-
-    private async UniTaskVoid LoadGameplayScene()
-    {
-        var scene = await this.GetSystem<IAssetSystem>().LoadSceneAsync(_gameplayScene, LoadSceneMode.Additive);
-        if (scene.Scene == null)
-        {
-            Debug.LogError("Failed to load gameplay scene.");
-            return;
-        }
-        while (scene.Scene.isLoaded == false)
-        {
-            await UniTask.Yield();
-        }
-        Debug.Log("Gameplay scene loaded successfully.");
+        _sceneLoader.OnInit();
         DestroyImmediate(gameObject);
     }
-
     public IArchitecture GetArchitecture()
     {
         return AppArchitecture_Rename.Interface;
