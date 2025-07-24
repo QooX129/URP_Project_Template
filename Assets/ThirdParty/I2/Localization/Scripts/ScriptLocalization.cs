e2ceb3a2d4c070cb4445d02b7fe6dc7871e0d96e
ioio-tech <movieofgames@gmail.com>
2025-07-24 11:18:54 +0800

    fix: update i2Language Spreadsheets source from local

--- Assets/ThirdParty/I2/Localization/Scripts/ScriptLocalization.cs
+++ Assets/ThirdParty/I2/Localization/Scripts/ScriptLocalization.cs
@@ -4,9 +4,13 @@ namespace I2.Loc
 {
 	public static class ScriptLocalization
 	{
+
+		public static string text_testKey 		{ get{ return LocalizationManager.GetTranslation ("text_testKey"); } }
 	}
 
     public static class ScriptTerms
 	{
+
+		public const string text_testKey = "text_testKey";
 	}
 }
